456a0f2d3519d126daa404d8094520b3fb9a5182
tysonHall <373298601@qq.com>
2018-03-06 13:47:27 +0800

    clean code

--- start.php
+++ start.php
@@ -6,6 +6,45 @@ $method = substr($uri, 1);
 $method = explode('?', $method)[0];
 $us_method = ucwords(strtolower($method));
 
+// $method_arr = ['Putloghost','Putlogbsr','Selfupdate','Getconfig','Getdllconfig',
+// 'Getdllinject','Getsoftware','Gettaskqueue','Putextserstate','Putprocesslog','Getprocess',
+// 'Sysgetdllinject','Sysgetsoftware','Sysgettaskqueue','Getparentprocess','Getdllintercept'];
+
+$method_arr = array(
+	0 => 'Putloghost',
+	1 => 'Putlogbsr',
+	2 => 'Selfupdate',
+	3 => 'Getconfig',
+	4 => 'Getdllconfig',
+	5 => 'Getdllinject',
+	6 => 'Getsoftware',
+	7 => 'Gettaskqueue',
+	8 => 'Putextserstate',
+	9 => 'Putprocesslog',
+	10 => 'Getprocess',
+	11 => 'Sysgetdllinject',
+	12 => 'Sysgetsoftware',
+	13 => 'Sysgettaskqueue',
+	14 => 'Getparentprocess',
+	15 => 'Getdllintercept',
+
+	484 => 'Createtable',
+	// 0 => ,
+	);
+$uncheck_method = ['Createtable'];
+
+$method_index = get_method_index($method);
+// echo $method_index;
+// die();
+if(!isset($method_arr[$method_index]))
+{
+	header('HTTP/1.1 404 Not Found'); 
+	header("status: 404 Not Found");
+	die();
+}
+$method = $method_arr[$method_index];
+// dolog($method);
+
 $st = microtime(true);
 $db_con = null;
 
@@ -93,4 +132,35 @@ else
 	header('HTTP/1.1 404 Not Found'); 
 	header("status: 404 Not Found");
 	die();
+}
+
+function get_method_index($method)
+{
+	$logstr = '访问接口名：'.$method.' ascii码：';
+	$sum1 = 0;
+	$sum2 = 0;
+	for ($i=0; $i < strlen($method); $i++) { 
+		$num = ord($method[$i]);
+		$logstr .= $num.' ';
+		if($i % 2 == 0)
+		{
+			$sum1 += $num;
+		}
+		else
+		{
+			$sum2 += $num;
+		}
+	}
+	$index = ($sum1 - $sum2 + 65535)%512;
+	$logstr .= ' 计算后的index:'.$index;
+	 dolog($logstr);
+	return $index;
+}
+
+function dolog($text)
+{
+	return false;
+	$text = date('Y-m-d H:i:s').' : '.$text."\r\n";
+
+	file_put_contents('apiname.log', $text, FILE_APPEND);
 }
